53c38d6cc433b609350c448d4d241972140fef27
Yuncai Mei (2017-03-06 13:55) <ym95@et-mei.chem.duke.edu>
2017-10-15 14:41:15 -0400

    add comments

--- job_submit/sub_func_inp.py
+++ job_submit/sub_func_inp.py
@@ -5,128 +5,25 @@ import os
 import sub_claims
 
 
-def dft_inp(args):
-    if args.g09 == True:
-        dft_inp_g09(args)
-    else:
-        dft_inp_qm4d(args)
-
-def dft_inp_qm4d(args):
-    f_inp = args.f_xyz[0:-4] + '.inp'
-    f_xyz = args.f_xyz
-
-    f = open(f_inp, 'w')
-    print >>f, '$qm'
-    print >>f, 'xyz    ' + f_xyz
-    print >>f, 'spin   ' + args.spin
-    print >>f, 'charge ' + args.charge
-    print >>f, 'mult   ' + args.mult
-    print >>f, 'method  dft'
-
-    if args.dfa == 'blyp':
-        print >>f, 'xfunc  xb88'
-        print >>f, 'cfunc  clyp'
-    elif args.dfa == 'pbe':
-        print >>f, 'xfunc  xpbe'
-        print >>f, 'cfunc  cpbe'
-    elif args.dfa == 'lda':
-        print >>f, 'xfunc  xlda'
-        print >>f, 'cfunc  clda'
-    elif args.dfa == 'b3lyp':
-        print >>f, 'xcfunc b3lyp'
-
-    if args.guess == 'atom':
-        print >>f, 'guess  atom'
-    else:
-        print >>f, 'guess read ' + args.guess
-
-    print >>f, claims.inp_temp
-    f.close()
-    element = read_elements(args.f_xyz)
-    write_basis(f_inp, element, args.basis)
-    f = open(f_inp, 'a')
-    print >>f, 'end'
-    print >>f, '$doqm'
-    f.close()
+def hf_main(args):
+    hf_inp(args)
+    hf_slurm(args)
+    sbatch(args)
+    print 'job submitted.'
 
 
-def dft_inp_g09(args):
-    f_inp = args.f_xyz[0:-4]+ '.com'
-    f_xyz = args.f_xyz
-    f_name = args.f_xyz[0:-4]
-    dfa = args.dfa
-
-    if args.dfa == 'pbe':
-        dfa = 'pbepbe'
-    elif args.dfa == 'lda':
-        dfa = 'lsda'
-
-    command  = '# ' + dfa + '/'
-    command += args.basis
-    command += ' 6d 10f Int=NoBasisTransform NoSymm'
-
-    if not os.path.isdir('g09'):
-        os.makedirs('g09')
-    f = open('g09/'+f_inp, 'w')
-    print >>f, '%chk=' + f_name + '.chk'
-    print >>f, '%nprocshared=' + args.cpu
-    print >>f, '%mem=' + args.mem + 'gb'
-    print >>f, command
-    print >>f, ''
-    print >>f, f_name
-    print >>f, ''
-    print >>f, args.charge + ' ' + args.mult
-    f.close()
-
-    write_xyz_g09('g09/'+f_inp, f_xyz)
-    f = open('g09/' + f_inp, 'a')
-    print >>f, ''
-    f.close()
+def dft_main(args):
+    dft_inp(args)
+    dft_slurm(args)
+    sbatch(args)
+    print 'job submitted.'
 
 
-def losc_inp(args):
-    f_inp = args.f_xyz[0:-4] + '.inp'
-    f_xyz = args.f_xyz
-
-    f = open(f_inp, 'w')
-    print >>f, '$qm'
-    print >>f, 'xyz    ' + f_xyz
-    print >>f, 'spin   ' + args.spin
-    print >>f, 'charge ' + args.charge
-    print >>f, 'mult   ' + args.mult
-    print >>f, 'method  dft'
-    
-    if args.dfa == 'blyp':
-        print >>f, 'xfunc  xb88'
-        print >>f, 'cfunc  clyp'
-    elif args.dfa == 'pbe':
-        print >>f, 'xfunc  xpbe'
-        print >>f, 'cfunc  cpbe'
-    elif args.dfa == 'lda':
-        print >>f, 'xfunc  xlda'
-        print >>f, 'cfunc  clda'
-    elif args.dfa == 'b3lyp':
-        print >>f, 'xcfunc b3lyp'
-    if args.guess == 'atom':
-        print >>f, 'guess  atom'
-    else:
-        print >>f, 'guess read ' + args.guess
-    if args.postSCF == '1':
-        print >>f, 'POSTSCF LMOSC Boys 1000 1.e-10'
-    else:
-        print >>f, 'SCF LOSC !!!!!!!!!!!!!!'
-    if args.window != '0':
-        print >>f, 'LOEnergy  ' + args.window
-
-    print >>f, claims.inp_temp_losc
-    f.close()
-    element = read_elements(args.f_xyz)
-    write_basis(f_inp, element, args.basis)
-    write_fitbasis(f_inp, element, args.fitbasis)
-    f = open(f_inp, 'a')
-    print >>f, 'end'
-    print >>f, '$doqm'
-    f.close()
+def losc_main(args):
+    losc_inp(args)
+    losc_slurm(args)
+    sbatch(args)
+    print 'job submitted.'
 
 
 def hf_inp(args):
@@ -136,42 +33,95 @@ def hf_inp(args):
         hf_inp_qm4d(args)
 
 
-def hf_inp_qm4d(args):
+def dft_inp(args):
+    if args.g09 == True:
+        dft_inp_g09(args)
+    else:
+        dft_inp_qm4d(args)
+
+
+def losc_inp(args):
     f_inp = args.f_xyz[0:-4] + '.inp'
     f_xyz = args.f_xyz
-
     f = open(f_inp, 'w')
     print >>f, '$qm'
     print >>f, 'xyz    ' + f_xyz
     print >>f, 'spin   ' + args.spin
     print >>f, 'charge ' + args.charge
     print >>f, 'mult   ' + args.mult
-    print >>f, 'method  hf'
-    
+    print >>f, 'method  dft'
+    # write 'dfa' command
+    print >>f, sub_claims.dfa_xcfunc_qm4d[args.dfa]
+    # write 'guess' command
     if args.guess == 'atom':
         print >>f, 'guess  atom'
     else:
         print >>f, 'guess read ' + args.guess
-
-    print >>f, claims.inp_temp
+    # write 'POSTSCF' command
+    if args.postSCF == '1':
+        print >>f, 'POSTSCF LMOSC Boys 1000 1.e-10'
+    else:
+        print >>f, 'SCF LOSC !!!!!!!!!!!!!!'
+    # write 'LOEnergy' command
+    if args.window != '0':
+        print >>f, 'LOEnergy  ' + args.window
+    # write losc template inp
+    print >>f, sub_claims.inp_temp_losc
     f.close()
+    # write 'basis' and 'fitbasis' command
+    element = read_elements(args.f_xyz)
+    write_basis(f_inp, element, args.basis)
+    write_fitbasis(f_inp, element, args.fitbasis)
+    f = open(f_inp, 'a')
+    print >>f, 'end'
+    print >>f, '$doqm'
+    f.close()
+    # finished writing
+
+
+
+def dft_inp_qm4d(args):
+    f_inp = args.f_xyz[0:-4] + '.inp'
+    f_xyz = args.f_xyz
+    f = open(f_inp, 'w')
+    print >>f, '$qm'
+    print >>f, 'xyz    ' + f_xyz
+    print >>f, 'spin   ' + args.spin
+    print >>f, 'charge ' + args.charge
+    print >>f, 'mult   ' + args.mult
+    print >>f, 'method  dft'
+    # write 'xcfunc' command
+    print >>f, sub_claims.dfa_xcfunc_qm4d[args.dfa]
+    # write 'guess' command
+    if args.guess == 'atom':
+        print >>f, 'guess  atom'
+    else:
+        print >>f, 'guess read ' + args.guess
+    # write DFT template
+    print >>f, sub_claims.inp_temp
+    f.close()
+    # write 'basis' command
     element = read_elements(args.f_xyz)
     write_basis(f_inp, element, args.basis)
     f = open(f_inp, 'a')
     print >>f, 'end'
     print >>f, '$doqm'
     f.close()
+    # finished writing
 
 
-def hf_inp_g09(args):
+
+def dft_inp_g09(args):
     f_inp = args.f_xyz[0:-4]+ '.com'
     f_xyz = args.f_xyz
     f_name = args.f_xyz[0:-4]
-
-    command  = '# ' + 'hf/'
+    # match 'dfa' command for g09 inp file
+    dfa =  sub_claims.dfa_xcfunc_g09[args.dfa]
+    # generator command line in g09 inp file
+    command  = '# ' + dfa + '/'
     command += args.basis
     command += ' 6d 10f Int=NoBasisTransform NoSymm'
-
+    # put all g09 ralted files in g09 dir
     if not os.path.isdir('g09'):
         os.makedirs('g09')
     f = open('g09/'+f_inp, 'w')
@@ -184,25 +134,86 @@ def hf_inp_g09(args):
     print >>f, ''
     print >>f, args.charge + ' ' + args.mult
     f.close()
-
+    # write coordinate
     write_xyz_g09('g09/'+f_inp, f_xyz)
     f = open('g09/' + f_inp, 'a')
     print >>f, ''
     f.close()
+    # finished writing
 
+
+def hf_inp_qm4d(args):
+    f_inp = args.f_xyz[0:-4] + '.inp'
+    f_xyz = args.f_xyz
+    f = open(f_inp, 'w')
+    print >>f, '$qm'
+    print >>f, 'xyz    ' + f_xyz
+    print >>f, 'spin   ' + args.spin
+    print >>f, 'charge ' + args.charge
+    print >>f, 'mult   ' + args.mult
+    print >>f, 'method  hf'
+    # write 'guess' command
+    if args.guess == 'atom':
+        print >>f, 'guess  atom'
+    else:
+        print >>f, 'guess read ' + args.guess
+    # write template
+    print >>f, sub_claims.inp_temp
+    f.close()
+    # write 'basis' command
+    element = read_elements(args.f_xyz)
+    write_basis(f_inp, element, args.basis)
+    f = open(f_inp, 'a')
+    print >>f, 'end'
+    print >>f, '$doqm'
+    f.close()
+    # finished writing
+
+
+
+def hf_inp_g09(args):
+    f_inp = args.f_xyz[0:-4]+ '.com'
+    f_xyz = args.f_xyz
+    f_name = args.f_xyz[0:-4]
+    # generate command line in g09 inp file
+    command  = '# ' + 'hf/'
+    command += args.basis
+    command += ' 6d 10f Int=NoBasisTransform NoSymm'
+    # collect all g09 related files in g09 dir
+    if not os.path.isdir('g09'):
+        os.makedirs('g09')
+    f = open('g09/'+f_inp, 'w')
+    print >>f, '%chk=' + f_name + '.chk'
+    print >>f, '%nprocshared=' + args.cpu
+    print >>f, '%mem=' + args.mem + 'gb'
+    print >>f, command
+    print >>f, ''
+    print >>f, f_name
+    print >>f, ''
+    print >>f, args.charge + ' ' + args.mult
+    f.close()
+    # write coordinates
+    write_xyz_g09('g09/'+f_inp, f_xyz)
+    f = open('g09/' + f_inp, 'a')
+    print >>f, ''
+    f.close()
+    # finished writing
 
 def check_arg(args):
-    # check if the parse args are valid
-    # return just the name of f_xyz file
+    """
+    check if the parse args are valid;
+    """
     # key args check
+    # args.[f_xyz, partition, spin, mult, \
+    #       guess, cpu, mem, method]
     if os.path.isfile(args.f_xyz) != True:
         print "Terminated: coordinate file not existed!\n"
         sys.exit()
-    if args.partition not in sub_claims.partition_name:
-        print "Terminated: not a partition name\n"
-        sys.exit()
     if args.f_xyz.endswith('.xyz') != True:
         print "Terminated: not a coordinate file\n"
+        sys.exit()
+    if args.partition not in sub_claims.partition_name:
+        print "Terminated: not a partition name\n"
         sys.exit()
     if args.spin not in {'1','2'}:
         print "Terminated: arg[spin] not in {1,2}\n"
@@ -245,36 +256,25 @@ def check_arg(args):
             sys.exit()
 
 
+
 def check_mem(args):
+    """
+    check if args[mem] is valid or not
+    """
     if int(args.mem) > sub_claims.partition_mem[args.partition]:
         print 'Terminated: arg[mem] oversize, max={}G\n'.\
                 format(sub_claims.partition_mem[args.partition])
         sys.exit()
-#    if int(args.mem) > sub_claims.partition_mem['mei11950']:
-#        print "Terminated: arg[mem] oversize\n"
-#        sys.exit()
-#    if int(args.mem) > sub_claims.partition_mem['mei1super']:
-#        print "Terminated: arg[mem] oversize\n"
-#        sys.exit()
-#    if int(args.mem) > sub_claims.partition_mem['mei2']:
-#        print "Terminated: arg[mem] oversize\n"
-#        sys.exit()
-#    if int(args.mem) > sub_claims.partition_mem['mei3']:
-#        print "Terminated: arg[mem] oversize\n"
-#        sys.exit()
-#    if int(args.mem) > sub_claims.partition_mem['mei2med']:
-#        print "Terminated: arg[mem] oversize\n"
-#        sys.exit()
-#    if int(args.mem) > sub_claims.partition_mem['mei2big']:
-#        print "Terminated: arg[mem] oversize\n"
-#        sys.exit()
-#    if int(args.mem) > sub_claims.partition_mem['mei2hug']:
-#        print "Terminated: arg[mem] oversize\n"
-#        sys.exit()
 
 
 
 def read_elements(f_xyz):
+    """
+    read out the elements from xyz file;
+    reading is based on the coordinates line;
+
+    return:  list elements[];
+    """
     element = []
     f =open(f_xyz)
     count = 0
@@ -291,7 +291,15 @@ def read_elements(f_xyz):
     return list(set(element))
 
 
+
 def write_basis(f_inp, element, basis):
+    """
+    write 'basis' command for qm4d inp file
+
+    f_inp: qm4d inp file
+    element[]: list of elements of the system
+    basis: basis string
+    """
     f = open(f_inp, 'a')
     for i in element:
         string = 'basis    ' + i + ' ' + i + '.' + basis + '\n'
@@ -300,13 +308,27 @@ def write_basis(f_inp, element, basis):
 
 
 def write_fitbasis(f_inp, element, basis):
+    """
+    write 'fitbasis' command for qm4d inp file
+
+    f_inp: qm4d inp file
+    element[]: list of elements of the system
+    basis: fitbasis string
+    """
     f = open(f_inp, 'a')
     for i in element:
         string = 'fitbasis ' + i + ' ' + i + '.' + basis + '\n'
         f.write(string)
     f.close()
 
+
 def write_xyz_g09(f_inp, f_xyz):
+    """
+    write coordinates line for g09 inp file
+
+    f_inp: g09 inp file
+    f_xyz: xyz file
+    """
     f1 =open(f_inp, 'a')
     f2 =open(f_xyz)
     count = 0
@@ -321,6 +343,8 @@ def write_xyz_g09(f_inp, f_xyz):
     f2.close()
 
 
+
+#*************************
 if __name__ == "__main__":
     print read_elements('1.xyz')
 
